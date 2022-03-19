0899293facace8c998ad54e116eef268cce59dec
duyphaphach <duyphamle97@gmail.com>
2022-03-19 20:46:34 +0700

    First working version

--- screen-capture.js
+++ screen-capture.js
@@ -9,21 +9,20 @@ setInterval(async () => {
     const pathDashboardSnapshots = `./snapshots/dashboard-${timestamp}.jpg`;
     const pathTaskManagerSnapshots = `./snapshots/task-manager-${timestamp}.jpg`;
     const errorCallback = (error) => (error ? console.log(error) : console.log('Snapped'));
-
     await sharp(image)
       .extract({
-        left: 646, top: 296, width: 402, height: 440,
+        left: 675, top: 314, width: 265, height: 77,
       })
       .grayscale()
-      .resize(1500, undefined)
+      .resize(800, undefined)
       .toFile(pathDashboardSnapshots, errorCallback);
 
     await sharp(image)
       .extract({
-        left: 1125, top: 85, width: 770, height: 676,
+        left: 1030, top: 78, width: 776, height: 169,
       })
       .grayscale()
-      .resize(1500, undefined)
+      .resize(2500, undefined)
       .toFile(pathTaskManagerSnapshots, errorCallback);
   } catch (error) {
     console.log('Failed to capture screenshots');
